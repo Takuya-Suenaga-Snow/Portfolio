a7eb5a991ab6c8acbdcef2245e56de1b95eab512
Takuya-Suenaga-Snow <suenaga.takuya.snow@gmail.com>
2020-12-24 09:37:53 +0900

    機能追加

--- Database_Checker/other_database.php
+++ Database_Checker/other_database.php
@@ -3,29 +3,12 @@ session_start();  // セッションの開始
 if(!isset($_SESSION['username'])){  // ログイン状態の確認
     header('Location: Database_Editer.php');  // ログインページに移動
     exit;
-}else{
-    if(isset($_SESSION['password'])){  // セッションの確認
-        $username = $_SESSION['username'];
-        $password = $_SESSION['password'];
-    }
 }
-if(isset($_POST['logout'])){
-    header('Location: Database_Editer.php');  // ログインページに移動
-    $_SESSION = array();
-    session_destroy();
+if(isset($_POST['back'])){
+    header('Location: excution.php');  // 元のページに移動
+    unset($_SESSION['othername']);
+    unset($_SESSION['otherpass']);
     exit;
-}
-if(isset($_POST['move'])){
-    if($_POST['othername'] == ''){
-        $error3 = '！データベース名を入力してください！';
-    }else{
-        $error3 = '';
-    }
-    if($_POST['otherpass'] == ''){
-        $error4 = '！パスワードを入力してください！';
-    }else{
-        $error4 = '';
-    }
 }
 ?>
 
@@ -33,17 +16,14 @@ if(isset($_POST['move'])){
 <html lang='ja'>
 
 <head>
-<title>login_page</title>
+<title>other_database</title>
 </head>
 
 <body>
 <?php
-if(isset($_POST['move']) && $error3.$error4==''){
-    $_SESSION['othername'] = $_POST['othername'];
-    $_SESSION['otherpass'] = $_POST['otherpass'];
-    header('Location: other_database.php');  // 他のデータベースに移動
-}
 try{
+    $username = $_SESSION['othername'];
+    $password = $_SESSION['otherpass'];
     // DB接続設定
     $dbname = str_replace('-', '', $username);
     $dsn = 'mysql:dbname='.$dbname.'db;host=localhost';
@@ -118,8 +98,7 @@ try{
             }
         }
         
-    }
-
+    }    
 
 // 接続失敗
 }catch(PDOException $e){
@@ -146,17 +125,8 @@ $pdo = null;
 <input type='submit' name='data_check' value='データの参照'>
 </form><br><br>
 
-<h2>他のデータベースへ移動</h2>
 <form action='' method='post'>
-<label>データベース名<br><input type='text' name='othername'></label>
-<?php if(isset($error3)){echo $error3;}?><br>
-<label>パスワード<br><input type='text' name='otherpass'></label>
-<?php if(isset($error4)){echo $error4;}?><br>
-<input type='submit' name='move' value='移動'><br>
-</form><br><br>
-
-<form action='' method='post'>
-<input type='submit' name='logout' value='ログアウト'>
+<input type='submit' name='back' value='戻る'>
 </form><br><hr>
 
 <?php
